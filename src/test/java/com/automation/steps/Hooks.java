30e2a2f77f277a9d3c3cf3294c6a70c688b81468
Billy Dashiev <dashievbilikto@gmail.com>
2024-05-01 22:34:22 -0400

    changed property file for Jenkins

--- src/test/java/com/automation/steps/Hooks.java
+++ src/test/java/com/automation/steps/Hooks.java
@@ -12,7 +12,16 @@ public class Hooks {
     @Before
     public void setUp() throws MalformedURLException {
         ConfigReaderUtils.initProperties();
-        if(ConfigReaderUtils.getProperty("execution.platform").equals("local")) {
+
+        //Jenkins parameter
+        String environment = System.getProperty("execution.platform");
+
+        // if we do not get parameters from Jenkins, consider config file parameter
+        if(environment == null) {
+            environment = ConfigReaderUtils.getProperty("execution.platform");
+        }
+
+        if(environment.equals("local")) {
             DriverUtils.createLocalDriver();
         } else {
             DriverUtils.createBrowserStackDriver();
